48ee3d5ed2d383c77eb451e866d1ad124a666eb1
MikeHampf <hampfma5@gmail.com>
2023-11-15 12:35:51 -0500

    start DVD logo

---
+++ dvdLogo.py
@@ -0,0 +1,20 @@
+import sys, random, time
+
+try: 
+    import bext
+except ImportError:
+    print("This program requires the bext module, which you")
+    print("can install by following the instructions at")
+    print("https://pypi.org/project/Bext/")
+    sys.exit()
+
+WIDTH, HEIGHT = bext.size()
+WIDTH -= 1
+NUMBER_OF_LOGOS = 5
+PAUSE_AMOUNT = 0.2
+COLORS = ['red','green','yellow','blue','magenta','cyan','white']
+UP_RIGHT = 'ur'
+UP_LEFT = 'ul'
+DOWN_RIGHT = 'dr'
+DOWN_LEFT = 'dl'
+DIRECTIONS = (UP_RIGHT,UP_LEFT,DOWN_RIGHT,DOWN_LEFT)
