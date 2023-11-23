9bb8bf75081777925f607a4c0d2998945f935ab9
MikeHampf <hampfma5@gmail.com>
2023-11-23 13:51:21 -0500

    continue calendar_maker

--- calendar_maker.py
+++ calendar_maker.py
@@ -28,3 +28,21 @@ while True:
         break
     print('Please enter a number from 1 to 12')
 
+def getCalendarfor(year, month):
+    calText=''
+    calText+=(' '*34)+MONTHS[month-1]+' '+str(year)+'\n'
+    calText+='...Sunday.....Monday....Tuesday...Wednesday...Thursday....Friday....Saturday..\n'
+    weekSeparator=('+----------'*7)+'+\n'
+    blankRow=('|          '*7)+'|\n'
+    currentDate = datetime.date(year, month, 1)
+    while currentDate.weekday()!=6:
+        currentDate-=datetime.timedelta(days=1)
+    
+    while True:
+        calText+=weekSeparator
+        dayNumberRow=''
+        for i in range(7):
+            dayNumberLabel = str(currentDate.day).rjust(2)
+            dayNumberRow += '|'+dayNumberLabel+(' '*8)
+            currentDate+=datetime.timedelta(days=1)
+        dayNumberRow += '|\n'
