37650dde484ae1f0c4f0e9efa5ca6fcbb55f1bec
MikeHampf <hampfma5@gmail.com>
2023-12-02 08:04:48 -0500

    finish countdown

--- countdown.py
+++ countdown.py
@@ -1,25 +1,42 @@
 import sys,time
 import sevseg
 
-secondsLeft=30
+while True:
+    print("How Long's Your Countdown?")
+    secondsLeft = input('> ')
+    if secondsLeft.isdecimal():
+        break
+    else:
+        print('Enter numbers only.')
+        continue
 
 try:
     while True:
         print('\n'*60)
         hours = str(secondsLeft // 360)
+        minutes = str((secondsLeft%3600)//60)
+        seconds=str(secondsLeft%60)
 
+        hDigits = sevseg.getSevSegStr(hours, 2)
+        hTopRow, hMiddleRow, hBottomRow = hDigits.splitlines()
 
-    print(hTopRow+'    '+mTopRow+'    '+sTopRow)
-    print(hMiddleRow+' * '+mMiddleRow+' * '+sMiddleRow)
-    print(hBottomRow+' * '+mBottomRow+' * '+sBottomRow)
+        mDigits = sevseg.getSevSegStr(minutes,2)
+        mTopRow, mMiddleRow, mBottomRow = mDigits.splitlines()
 
-    if secondsLeft==0:
+        sDigits = sevseg.getSevSegStr(seconds,2)
+        sTopRow, sMiddleRow, sBottomRow = sDigits.splitlines()
+
+        print(hTopRow+'    '+mTopRow+'    '+sTopRow)
+        print(hMiddleRow+' * '+mMiddleRow+' * '+sMiddleRow)
+        print(hBottomRow+' * '+mBottomRow+' * '+sBottomRow)
+
+        if secondsLeft==0:
+            print()
+            print('    ****BOOM****')
+            break
+
         print()
-        print('    ****BOOM****')
-        break
-
-    print()
-    print('Press Ctrl-C tp quit.')
+        print('Press Ctrl-C tp quit.')
 
 except KeyboardInterrupt:
     print('Countdown, by A. Sweigart')
