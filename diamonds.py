635a5a3fc09f0d70b94abe7daf31bf3a33137a65
MikeHampf <hampfma5@gmail.com>
2023-12-04 14:17:54 -0500

    start diamonds

---
+++ diamonds.py
@@ -0,0 +1,35 @@
+r"""Diamonds, by A. Sweigart
+Draws diamonds of various sizes.
+View this code at https://nostarch.com/big-book-small-python-projects
+                           /\       /\
+                          /  \     //\\
+            /\     /\    /    \   ///\\\
+           /  \   //\\  /      \ ////\\\\
+ /\   /\  /    \ ///\\\ \      / \\\\////
+/  \ //\\ \    / \\\///  \    /   \\\///
+\  / \\//  \  /   \\//    \  /     \\//
+ \/   \/    \/     \/      \/       \/
+  """
+
+def main():
+    print('Diamonds, by A. Sweigart')
+    for diamondSize in range(0,6):
+        displayOutlineDiamond(diamondSize)
+        print()
+        displayFilledDiamond(diamondSize)
+        print()
+
+def displayOutlineDiamond(size):
+    for i in range(size):
+        print(' '*(size-i-1), end='')
+        print('/', end='')
+        print(' '*(i*2), end='')
+        print('\\')
+    for i in range(size:
+        print(' '*i, end='')
+        print('\\', end='')
+        print(' '*((size-i-1)*2), end='')
+        print('/')
+
+if __name__=='__main__':
+    main()
